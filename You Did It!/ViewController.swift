af1818bca164e076c00f426047cba0b4509f57ea
BSD-06 <joshtesfazghji@gmail.com>
2016-11-21 13:23:38 +0800

    app

--- You Did It!/ViewController.swift
+++ You Did It!/ViewController.swift
@@ -7,12 +7,44 @@
 //
 
 import UIKit
-
+import AVFoundation
 class ViewController: UIViewController {
 
+
+    
+    
+     var audioPlayer = AVAudioPlayer()
+    
+    var isPlaying = false
+    var timer:NSTimer!
+    
+   // var audioPath = NSURL(fileURLWithPath: NSBundle().pathForResource("cheer", ofType: "mp3")!)
+    
+    //var playPauseBtn = UIButton()
+        
+    //var musicPaused: Bool = false
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+//        do{
+//       
+//        audioPlayer = try AVAudioPlayer(contentsOfURL: audioPath)
+//        audioPlayer.prepareToPlay()
+//        }catch{
+//            print("no worky");
+//        }
+        
+        
+        //let music = Bundle.main.path(forResource: "cheer", ofType: "mp3")
+        // copy this syntax, it tells the compiler what to do when action is received
+        
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +53,47 @@ class ViewController: UIViewController {
     }
 
 
+    
+    
+    @IBAction func yayButton(sender: AnyObject) {
+        
+    
+      // this works
+        
+        let cheerSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("cheer", ofType: "mp3")!)
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOfURL: cheerSound)
+            audioPlayer.prepareToPlay()
+            print("audio playing")
+        } catch {
+            print("Problem in getting File")
+        }
+       
+//        
+//        if isPlaying {
+//            audioPlayer.pause()
+//            isPlaying = false
+//        } else {
+//            audioPlayer.play()
+//            isPlaying = true
+//            
+//            timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: "updateTime", userInfo: nil, repeats: true)
+//        }
+        
+        
+      
+        
+        
+        
+        
+        
+        
+        
+        
+        //
+    }
+    
+    
+    
 }
 
